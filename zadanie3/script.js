1eb7fb28a9303a078efeec720a5f9d87f7141f8c
ppacholek <paulina.pacholek@o2.pl>
2019-12-09 21:29:46 +0100

    add functionality to hamburger button

--- zadanie3/script.js
+++ zadanie3/script.js
@@ -1,1 +1,10 @@
-console.log('Hello World')
+const hamburgerMenuButton = document.querySelector('.hamburger-menu-button');
+const pageNavigation = document.querySelector('.page-navigation');
+
+hamburgerMenuButton.addEventListener('click', (e) => {
+    if (pageNavigation.classList.contains('hidden-on-mobile')) {
+        pageNavigation.classList.remove('hidden-on-mobile')
+    } else {
+        pageNavigation.classList.add('hidden-on-mobile')
+    }
+})
